5b90ea68fd4d0ad319440e9ad24e97ed9b7f5ee7
yash987 <ys2599518@gmail.com>
2024-08-21 16:58:38 +0530

    some change

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,6 @@ import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
